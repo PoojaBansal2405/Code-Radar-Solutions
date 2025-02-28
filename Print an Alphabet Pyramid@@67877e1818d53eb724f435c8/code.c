1c33236cbe951b5fd32fab1f588907be5d489003
Pooja Bansal <poojabansal.2405@gmail.com>
2025-02-28 20:45:30 +0530

    Solution to Print an Alphabet Pyramid

---
+++ Print an Alphabet Pyramid@@67877e1818d53eb724f435c8/code.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    for(int i=0; i<=N; i++){
+        for(int j=0; j<=N ;j++){
+            printf("%c",'A'+j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
