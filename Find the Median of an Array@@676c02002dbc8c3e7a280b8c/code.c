47d1afab7b64c322705dca1db12e17838c41c736
Pooja Bansal <poojabansal.2405@gmail.com>
2025-04-11 00:05:04 +0530

    Solution to Find the Median of an Array

--- Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
+++ Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
@@ -16,9 +16,9 @@ for(i=0;i<n;i++){
     }
 }
 if(n%2!=0){
-    printf("%d",arr[(n+1)/2]);
+    printf("%d",arr[(n-1)/2]);
 }
 else{
-    printf("%d",(arr[(n/2)]+arr[((n/2)+1)])/2);
+    printf("%d",(arr[((n-2)/2)]+arr[((n/2))])/2);
 }
 }
